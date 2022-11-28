bf3ffc3d9e4a6aedeea9b6c44384d56b6e545cec
leconmat <leconmat@m16020.phelma.grenoble-inp.fr>
2022-11-28 11:59:38 +0100

    Added basic gates and test file with FA

--- inc/defs.h
+++ inc/defs.h
@@ -4,14 +4,16 @@
 using namespace std;
 
 struct Node {
+protected:
 	vector<Node*> children;
 	vector<bool> values;
-
+public:
 	Node(vector<Node*> _children = {}) : children(_children) {}
 	Node(vector<bool> _values = {}) : values(_values) {}
 	virtual bool computeOut() = 0;
 
 	bool getValue() { return values.back(); }
+	void setValue(bool value) { values.push_back(value); }
 };
 
 struct Output : public Node {
@@ -39,6 +41,76 @@ struct And : public Node {
 
 	bool computeOut() {
 		values.push_back(children.front()->computeOut() && children.back()->computeOut());
+		return values.back();
+	}
+};
+
+struct Or : public Node {
+
+	Or(vector<Node*> _children = {}) : Node(_children) {}
+
+	bool computeOut() {
+		values.push_back(children.front()->computeOut() || children.back()->computeOut());
+		return values.back();
+	}
+};
+
+struct Xor : public Node {
+
+	Xor(vector<Node*> _children = {}) : Node(_children) {}
+
+	bool computeOut() {
+		values.push_back(children.front()->computeOut() ^ children.back()->computeOut());
+		return values.back();
+	}
+};
+
+struct Not : public Node {
+
+	Not(vector<Node*> _children = {}) : Node(_children) {}
+
+	bool computeOut() {
+		values.push_back(~(children.front()->computeOut()));
+		return values.back();
+	}
+};
+
+struct Nand : public Node {
+
+	Nand(vector<Node*> _children = {}) : Node(_children) {}
+
+	bool computeOut() {
+		values.push_back(~(children.front()->computeOut() && children.back()->computeOut()));
+		return values.back();
+	}
+};
+
+struct Nor : public Node {
+
+	Nor(vector<Node*> _children = {}) : Node(_children) {}
+
+	bool computeOut() {
+		values.push_back(~(children.front()->computeOut() || children.back()->computeOut()));
+		return values.back();
+	}
+};
+
+struct Xnor : public Node {
+
+	Xnor(vector<Node*> _children = {}) : Node(_children) {}
+
+	bool computeOut() {
+		values.push_back(~(children.front()->computeOut() ^ children.back()->computeOut()));
+		return values.back();
+	}
+};
+
+struct Mux : public Node {
+
+	Mux(vector<Node*> _children = {}) : Node(_children) {}
+
+	bool computeOut() {
+		values.push_back((children.at(0)->computeOut() && children.back()->computeOut()) || (children.at(1)->computeOut() && ~(children.back()->computeOut())));
 		return values.back();
 	}
 };
--- inc/test.cpp
+++ inc/test.cpp
@@ -3,11 +3,50 @@
 
 int main() {
 
-	struct Input in1({1}), in2({1});
-	struct And and2({&in1, &in2});
-	struct Output out({&and2});
+	/* FULL ADDER schematic */
+	struct Input in1({0}), in2({0}), cin({0});
+	struct Xor xor2_1({&in1, &in2}), xor2_2({&xor2_1, &cin});
+	struct And and2_1({&in1, &in2}), and2_2({&xor2_1, &cin});
+	struct Or or2({&and2_1, &and2_2});
+	struct Output out({&xor2_2});
+	struct Output c_out({&or2});
+	
 	out.computeOut();
-	cout << "out = " << out.values.back() << "\n";
-
+	cout << "out = " << out.getValue() << "\n";
+	in1.setValue(1);
+	in2.setValue(0);
+	cin.setValue(0);
+	out.computeOut();
+	cout << "out = " << out.getValue() << "\n";
+	in1.setValue(0);
+	in2.setValue(1);
+	cin.setValue(0);
+	out.computeOut();
+	cout << "out = " << out.getValue() << "\n";
+	in1.setValue(1);
+	in2.setValue(1);
+	cin.setValue(0);
+	out.computeOut();
+	cout << "out = " << out.getValue() << "\n";
+	in1.setValue(0);
+	in2.setValue(0);
+	cin.setValue(1);
+	out.computeOut();
+	cout << "out = " << out.getValue() << "\n";
+	in1.setValue(1);
+	in2.setValue(0);
+	cin.setValue(1);
+	out.computeOut();
+	cout << "out = " << out.getValue() << "\n";
+	in1.setValue(0);
+	in2.setValue(1);
+	cin.setValue(1);
+	out.computeOut();
+	cout << "out = " << out.getValue() << "\n";
+	in1.setValue(1);
+	in2.setValue(1);
+	cin.setValue(1);
+	out.computeOut();
+	cout << "out = " << out.getValue() << "\n";
 	return 0;
 }
