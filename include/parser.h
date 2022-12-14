fc5282cd1a44cad9e81ee2a2c0ac0956b007bf03
Romain_Plmg <plumaugat.romain78@gmail.com>
2022-12-14 15:33:36 +0100

    Parser done v1

--- include/parser.h
+++ include/parser.h
@@ -7,10 +7,30 @@
 #include <vector>
 #include <list>
 
+#include "defs.h"
+
+#define NODES_DICT_SIZE 10
+
 using namespace std;
 
+enum Structure {
+    S_Output,
+    S_Input,
+    S_And,
+    S_Or,
+    S_Xor,
+    S_Not,
+    S_Nand,
+    S_Nor,
+    S_Xnor,
+    S_Mux,
+    MISSING,
+};
+
 int wavedrom_parser(list<string> &waveFile);
-int signal_parser(list<string> &waveFile, vector<bool> &signal);
-int dot_parser(list<string> &dotFile);
+int signal_parser(list<string> &waveFile, vector<bool> &signal, vector<string> &nodesName);
+int dot_parser(list<string> &dotFile, vector<string> &nodesName);
+Structure resolveStructure(string input);
+int getIndex(vector<string> l, string str);
 
 #endif
